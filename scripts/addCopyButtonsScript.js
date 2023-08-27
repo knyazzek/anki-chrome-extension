bcb075c9a36f7b68e0a14fe2da365598474f44bc
Vladislav Averin <vaverin@griddynamics.com>
2023-08-27 17:27:21 +0400

    copy word's definition

--- scripts/addCopyButtonsScript.js
+++ scripts/addCopyButtonsScript.js
@@ -1,3 +1,5 @@
+//Add copy mp3 to clipboard button
+
 function addCopyMp3ToClipboardButton(audioElement) {
     const mp3Source = audioElement.getElementsByTagName("source")[0];
     const mp3Url =  "https://dictionary.cambridge.org/" + mp3Source.getAttribute("src");
@@ -5,7 +7,7 @@ function addCopyMp3ToClipboardButton(audioElement) {
     const playButton = audioElement.parentElement
 
     const copyButton = document.createElement("button");
-    copyButton.classList.add("copyAudioButton");
+    copyButton.classList.add("copyButton");
     copyButton.innerHTML = "&#10064;";
 
     // Add click event listener
@@ -23,3 +25,54 @@ function addCopyMp3ToClipboardButton(audioElement) {
 
 const audioElements = document.getElementsByTagName("audio");
 Array.from(audioElements).forEach(addCopyMp3ToClipboardButton);
+
+
+//Add copy definition to clipboard button
+
+function addCopyDefinitionToClipboardButton(definitionElement) {
+    const definitionContainerElement = definitionElement.parentElement
+
+    const copyButton = document.createElement("button");
+    copyButton.classList.add("copyButton");
+    copyButton.innerHTML = "&#10064;";
+
+    let definitionText = '';
+    let partOfSpeech = '';
+
+    // Copy definition text
+    definitionElement.childNodes.forEach(node => {
+        if (node.nodeType === Node.TEXT_NODE) {
+            definitionText += node.textContent;
+        } else if (node.tagName === 'A') {
+            definitionText += node.textContent;
+        }
+    });
+
+    // Copy part of speech
+    const workHeaderElement = definitionElement.closest(".pr.entry-body__el");
+    console.log(workHeaderElement)
+    const partOfSpeechElement = workHeaderElement.querySelector(".pos.dpos");
+    console.log(partOfSpeechElement)
+
+    if (partOfSpeechElement) {
+        partOfSpeech = partOfSpeechElement.childNodes[0].textContent
+        console.log(partOfSpeech)
+    }
+
+    const clipboardText = definitionText + partOfSpeech
+
+    // Add click event listener
+    copyButton.addEventListener("click", function() {
+      // Copy text to clipboard
+      navigator.clipboard.writeText(clipboardText).then(function() {
+        console.log("Text successfully copied");
+      }).catch(function(err) {
+        console.log("Unable to copy text", err);
+      });
+    });
+
+    definitionContainerElement.insertAdjacentElement("afterbegin", copyButton)
+}
+
+const definitionElements = document.getElementsByClassName("def");
+Array.from(definitionElements).forEach(addCopyDefinitionToClipboardButton);
