ac0e8f63b908633a4b1175f64c80866d69845ccf
Vladislav Averin <vaverin@griddynamics.com>
2023-08-28 10:59:53 +0400

    rename wrong variable

--- scripts/addCopyButtonsScript.js
+++ scripts/addCopyButtonsScript.js
@@ -39,9 +39,9 @@ function addCopyDefinitionToClipboardButton(definitionElement) {
     definitionText = getElementsTextWithoutTags(definitionElement)
 
     // Copy part of speech
-    const workHeaderElement = definitionElement.closest(".entry-body");
-    console.log(workHeaderElement)
-    const partOfSpeechElement = workHeaderElement.querySelector(".pos.dpos");
+    const wordHeaderElement = definitionElement.closest(".entry-body");
+    console.log(wordHeaderElement)
+    const partOfSpeechElement = wordHeaderElement.querySelector(".pos.dpos");
     console.log(partOfSpeechElement)
 
     if (partOfSpeechElement) {
