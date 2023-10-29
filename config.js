525a50bdb973424c0791f20c7ace5b4ab41338d8
Vladislav Averin <vaverin@griddynamics.com>
2023-10-29 18:15:16 +0400

    add config

---
+++ config.js
@@ -0,0 +1,10 @@
+const defaultSettings = {
+    cambridgeDictionaryUrl: "https://dictionary.cambridge.org",
+    wordRootElementClasses: [".entry-body__el", ".pr.idiom-block"],
+    wordTitleElementClass: ".di-title",
+    wordTypeElementClass: ".pos.dpos",
+    wordExampleElementClass: "eg",
+
+    copyButtonSign: "&#10064;",
+    copyButtonClickedColor: "#C0C0C0"
+}
--- scripts/addCopyButtonsScript.js
+++ scripts/addCopyButtonsScript.js
@@ -2,13 +2,13 @@
 function addCopyButton(textToCopy, element) {
     const copyButton = document.createElement("button");
     copyButton.classList.add("copyButton");
-    copyButton.innerHTML = "&#10064;";
+    copyButton.innerHTML = defaultSettings.copyButtonSign;
 
     copyButton.addEventListener("click", function() {
         // Copy text to clipboard
         navigator.clipboard.writeText(textToCopy).then(function() {
             console.log("Text successfully copied: " + textToCopy);
-            copyButton.style.color = "#C0C0C0";
+            copyButton.style.color = defaultSettings.copyButtonClickedColor;
         }).catch(function(err) {
             console.log("Unable to copy text", err);
         });
@@ -21,7 +21,7 @@ function addCopyButton(textToCopy, element) {
 // Add copy mp3 to clipboard
 function addCopyMp3ToClipboardButton(audioElement) {
     const mp3Source = audioElement.getElementsByTagName("source")[0];
-    const mp3Url =  "https://dictionary.cambridge.org/" + mp3Source.getAttribute("src");
+    const mp3Url =  defaultSettings.cambridgeDictionaryUrl + mp3Source.getAttribute("src");
 
     addCopyButton(mp3Url, audioElement.parentElement)
 }
@@ -36,15 +36,16 @@ function addCopyDefinitionToClipboardButton(definitionElement) {
     let wordDefinitionText = '';
     let wordType = '';
 
-    // Copy word
-    const wordRootElement = definitionElement.closest(".entry-body__el") || definitionElement.closest(".pr.idiom-block");
-    wordTitle = getElementsTextWithoutTags(wordRootElement.querySelector(".di-title"));
+    // Copy word's title
+    rootElementSelector = defaultSettings.wordRootElementClasses.join(", ")
+    const wordRootElement = definitionElement.closest(rootElementSelector);
+    wordTitle = getElementsTextWithoutTags(wordRootElement.querySelector(defaultSettings.wordTitleElementClass));
 
     // Copy word's definition
     wordDefinitionText = getElementsTextWithoutTags(definitionElement).trim();
 
     // Copy word's type
-    const wordTypeElement = wordRootElement.querySelector(".pos.dpos");
+    const wordTypeElement = wordRootElement.querySelector(defaultSettings.wordTypeElementClass);
 
     if (wordTypeElement) {
         wordType = wordTypeElement.childNodes[0].textContent
@@ -74,7 +75,7 @@ Array.from(definitionElements).forEach(addCopyDefinitionToClipboardButton);
 
 // Add copy word's example to clipboard
 function addCopyExampleToClipboardButton(exampleElement) {
-    exampleTextElement = exampleElement.getElementsByClassName("eg")[0]
+    exampleTextElement = exampleElement.getElementsByClassName(defaultSettings.wordExampleElementClass)[0]
     exampleText = getElementsTextWithoutTags(exampleTextElement)
 
     addCopyButton(exampleText, exampleElement)
